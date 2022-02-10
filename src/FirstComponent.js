8ac723327002185e731e42198003b98c743467f2
Diana Marcelin <dianamarcelin@Dianas-MacBook-Air.local>
2022-02-10 13:20:15 -0500

    first react app

---
+++ src/FirstComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function FirstComponent() {
+  return <h1>This is cool!</h1>;
+}
+
+export default FirstComponent;
+//exporting so component can be used somewhere else.
